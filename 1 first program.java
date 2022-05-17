07614adfff3b3032762e3a89c95460b23cdf5fb1
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-05-18 00:08:19 +0500

    first program

--- 1 first program.java
+++ 1 first program.java
@@ -1,5 +1,19 @@
 class MyClass {
     public static void main(String[ ] args) {
-        System.out.println("Rana Abobakar");
+        System.out.println("Rana");//print new line in end of string 
+    System.out.print("Abobakar");//not print new line
+
     }
 }
+
+/*In Java, every line of code that can actually run needs to be inside a class.
+In our example, we named the class MyClass. You will learn more about classes in the upcoming modules.
+
+In Java, each application has an entry point, or a starting point, which is a method called main. Along with main, the keywords public and static will also be explained later.
+
+- public: anyone can access it
+- static: method can be run without creating an instance of the class containing the main method
+- void: method doesn't return any value
+- main: the name of the method
+
+*/
