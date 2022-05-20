0aec99cdab46aa18ae792a2da31769ff638340e4
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-05-20 15:58:14 +0500

    calculate remaining amount

---
+++ 4 calculate test.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class calculte
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int amount = scanner.nextInt();
+		int remaining=0;
+		for(int i=0;i<3;i++)
+		{
+           remaining=amount/10;
+		   amount=amount-remaining;
+		   System.out.println("remainng "+remaining);
+		   System.out.println("amout "+amount);
+
+		}
+		System.out.println(amount);
+	}
+}
