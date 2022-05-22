b98fe56dc97b06e0c78e9a52686105f77d3bdfaa
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-05-23 01:50:26 +0500

    for loop

---
+++ for_loop_10.java
@@ -0,0 +1,11 @@
+public class for_loop_10 {
+    public static void main(String[] args) {
+
+//       ⁡⁢⁣⁣​‌‌‍  𝗝𝗮𝘃𝗮 𝗙𝗼𝗿 𝗟𝗼𝗼𝗽​⁡
+// When you know exactly how many times you want to loop through a 
+// block of code, use the for loop instead of a while loop:
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+          }  
+    }
+}
