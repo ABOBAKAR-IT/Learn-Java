08fc4af1924333ba0716f3c585c1f9cf1a777993
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-05-23 01:39:24 +0500

    while loop

---
+++ while_9.java
@@ -0,0 +1,16 @@
+public class while_9 {
+    public static void main(String[] args) {
+        //​‌‌‍⁡⁢⁣⁣𝙬𝙝𝙞𝙡𝙚 𝙡𝙤𝙤𝙥⁡​
+        int i = 0;
+        while (i < 5) {
+          System.out.println(i);
+          i++;
+        }  
+// ​‌‌‍⁡⁢⁣⁣𝙙𝙤 𝙬𝙝𝙞𝙡𝙚 𝙡𝙤𝙤𝙥⁡​
+        do {
+            System.out.println(i);
+            i++;
+          }
+          while (i < 5);
+    }
+}
