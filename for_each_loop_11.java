a74a22c366bfac3e83da070e900885624c626388
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-05-23 01:52:51 +0500

    for each loop

---
+++ for_each_loop_11.java
@@ -0,0 +1,12 @@
+public class for_each_loop_11 {
+    public static void main(String[] args) {
+//        ⁡⁢⁣⁣​‌‌‍ 𝗙𝗼𝗿-𝗘𝗮𝗰𝗵 𝗟𝗼𝗼𝗽​⁡
+// There is also a "for-each" loop, which is used exclusively to loop through elements in an array:
+
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+for (String i : cars) {
+  System.out.println(i);
+}    
+
+    }
+}
