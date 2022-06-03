d201a622445e0a0cbdc6c5b2a80c27c74089d2b1
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-06-03 15:24:52 +0500

    arraylist task done

---
+++ uni class test/arrayslist.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class arrayslist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+while (true) {
+    System.out.println("Enter a Student Marks: ");
+    int input=new java.util.Scanner(System.in).nextInt();
+    if(input==-1)
+    break;
+    if(input==2)
+    {
+        System.out.println(list);
+    }
+    list.add(input);
+}
+
+
+        // list.add("Hello");
+        // list.add("World");
+        // list.add("!");
+        // System.out.println(list);
+        // System.out.println(list.get(1));
+        // System.out.println(list.size());
+        // System.out.println(list.isEmpty());
+        // list.remove(1);
+        // System.out.println(list);
+        // list.add(1, "World");
+        // System.out.println(list);
+        // list.set(1, "World");
+        // System.out.println(list);
+        // list.clear();
+        // System.out.println(list);
+    }
+}
