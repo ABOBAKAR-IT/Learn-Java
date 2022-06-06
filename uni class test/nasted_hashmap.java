03dcfed0d633d60743c5f8e8da4372655169966f
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-06-06 11:05:52 +0500

    nested hashmap task done , enter sutent name , subject name and marks.

--- uni class test/nasted_hashmap.java
+++ uni class test/nasted_hashmap.java
@@ -4,15 +4,24 @@ public class nasted_hashmap {
     public static void main(String[] args) {
         HashMap<String, HashMap<String, String>> myMap = new HashMap<String, HashMap<String, String>>();
         HashMap<String, String> myMap2 = new HashMap<String, String>();
-        myMap2.put("C++", "88");
-        myMap2.put("marks", "75");
-        myMap.put("ABOBAKAR", myMap2);
-        myMap.put("tayyab", myMap2);
+        while (true) {
+            System.out.println("Enter a Student Name: ");
+            String name=System.console().readLine();
+            if(name.equals("-1"))
+            break;
+            System.out.println("Enter a Student Subject Name: ");
+            String subject=System.console().readLine();
+            System.out.println("Enter a Student Marks: ");
+            String marks=System.console().readLine();
+myMap2.put(subject, marks);
+myMap.put(name, myMap2);
+        }
+       
 
-        System.out.println(myMap.get("ABOBAKAR").get("C++"));
-        System.out.println(myMap.get("ABOBAKAR").get("marks"));
+        System.out.println(myMap.get("ABOBAKAR"));
+         System.out.println(myMap.get("ABOBAKAR").get("C++"));
 
-        System.out.println(myMap.get("tayyab").get("C++"));
-        System.out.println(myMap.get("tayyab").get("marks"));
+        // System.out.println(myMap.get("tayyab").get("C++"));
+        // System.out.println(myMap.get("tayyab").get("marks"));
     }
 }
